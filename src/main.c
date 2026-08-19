405bb9b74794017634d1d215e0b41e57afb2ba54
avishekkrmahto <avishekkrmahto01@gmail.com>
2026-08-19 02:31:02 +0000

    Added token code

--- src/main.c
+++ src/main.c
@@ -1,51 +1,59 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>          // Fixed: Changed "String.h" to lowercase "string.h"
+#include <stdio.h> 
+#include <stdlib.h> 
+#include <string.h>
 #include <readline/history.h>
 #include <readline/readline.h>
-#include "history.h"
+#include "token.h"
+#include "lexer.h"
+#include "parse.h"
+#include "expand.h"
+int main(void)
+{
+    // Display a welcome banner when the shell starts
+    printf("=====================================\n");
+    printf("      Shellforge \n");
+    printf(" A Unix Style Shell written in C\n");
+    printf("=====================================\n");
 
-int main(){
-    printf("----------------------------------------------------\n"); // Fixed: 'prinf' to 'printf'
-    printf("                  SHELL-FORGE                       \n");
-    printf("          A unix style shell written in C           \n");
-    printf("----------------------------------------------------\n"); // Fixed: 'prinf' to 'printf'
+  token_list_t tokens;
+ pipeline_t pipeline;
+ 
+ char *line;
 
-    // INITIALIZING HISTORY !!
-    using_history();
-
-    char *line;
-    while(1){
+    while (1)
+    {
         line = readline("shellforge$ ");
-        if(line == NULL){
-            printf("WRONG COMMAND !!\n");
+        if (line == NULL)
+        {
+            printf("\nGoodbye!\n");
             break;
         }
-        if(strlen(line) == 0){
-            free(line);      // Added: Free empty lines to prevent memory leaks
+        if (strlen(line) == 0)
+        {
+            free(line);
             continue;
         }
 
-	if(strcmp(line,"history")==0){
-	    print_history();
-	    free(line);
-	    continue;
-	}
+         add_history(line);
+	lexer(line, &tokens);
+        token_print(&tokens);
+        
 
-	add_history(line);
-	printf(" YOU ENTERED : %s\n",line);
-	if(strcmp(line,"exit") == 0){
-	    free(line);
-	    printf("Exiting...\n");
-	    break;
+	if(parse(&tokens, &pipeline))
+	{
+		expand_variables(&pipeline);
+    		pipeline_print(&pipeline);
 	}
+        
 
+
+        if (strcmp(line, "exit") == 0)
+        {
+            free(line);
+            printf("Exiting...\n");
+            break;
+        }
 	free(line);
-
-    }
+    }    
     return 0;
 }
-
-
-
-
